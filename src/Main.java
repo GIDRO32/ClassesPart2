6d6b83af1f99f3e0309810218c319344440ad038
GIDRO32 <pambossable@gmail.com>
2022-12-29 13:06:46 +0200

    Add project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+class GameType
+{
+    private String genre;
+    private int number;
+    public GameType(String genre, int number)
+    {
+        this.genre = genre;
+        this.number = number;
+    }
+
+    public void setGenre(String genre)
+    {
+        this.genre = genre;
+    }
+
+    public void setNumber(int number)
+    {
+        this.number = number;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void print()
+   {
+       System.out.println("This output has no sense: " + genre + number);
+   }
+}
+public class Main
+{
+    public static void main(String[] args)
+    {
+        GameType shooter = new GameType("shooter", 35);//Setting properties via constructor
+        shooter.setGenre("Random");//setting properties via method
+        shooter.setNumber(37);
+        shooter.print();
     }
 }
